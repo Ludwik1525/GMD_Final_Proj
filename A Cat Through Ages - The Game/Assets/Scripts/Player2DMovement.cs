a28a73c8f3cd96d192eaadab94bce7cabc7d6fd1
PeipeiSu <266128@via.dk>
2019-04-16 19:06:18 +0200

    2D and 3D movement scripts from last exercises, cat and city assets

---
+++ A Cat Through Ages - The Game/Assets/Scripts/Player2DMovement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2DMovement : MonoBehaviour {
+
+    [SerializeField] public float moveSpeed, jumpHeight;
+    [SerializeField] private Vector2 movement;
+    [SerializeField] private float moveHorizontal;
+
+    private Rigidbody2D player;
+    private Vector3 m_Velocity = Vector3.zero;
+    public bool isGrounded = true;
+//    public bool keyFunctionOpen;
+
+ //   public AudioSource source;
+ //   public AudioClip jump;
+
+
+    void Start()
+    {
+        player = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+
+ //       if (keyFunctionOpen)
+        {
+            if (Input.GetButtonDown("Jump") && isGrounded)
+            {
+                player.AddForce(new Vector2(0f, jumpHeight));
+ //               source.PlayOneShot(jump);
+            }
+
+            Vector3 targetVelocity = new Vector2(Input.GetAxis("Horizontal") * moveSpeed * Time.fixedDeltaTime * 10f,
+                player.velocity.y);
+            player.velocity = Vector3.SmoothDamp(player.velocity, targetVelocity, ref m_Velocity, 0.05f);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Ground" || col.gameObject.tag == "Player")
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Ground" || col.gameObject.tag == "Player")
+        {
+            isGrounded = false;
+        }
+    }
+}
---
+++ A Cat Through Ages - The Game/Assets/Scripts/Player3DMovement.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player3DMovement : MonoBehaviour {
+
+    public Rigidbody rb;
+
+    public float movementSpeed = 10;
+    public float rotationSpeed = 5;
+    public float rotationDegrees = 90;
+    public float runningSpeed = 30;
+    public float backwardsSpeed = 5;
+    public float jumpForce = 2;
+    public Vector3 jump;
+    public bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        jump = new Vector3(0.0f, 2.5f, 0.0f);
+    }
+
+    void OnCollisionStay()
+    {
+        isGrounded = true;
+    }
+
+    void FixedUpdate()
+    {
+        if (!Physics.Raycast(transform.position, -transform.up, 0.0001f))
+        {
+            isGrounded = false;
+        }
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            //moving forward and rotating to the left simultaneously
+            if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+            {
+                if (Input.GetKey(KeyCode.Space))
+                {
+                    rb.position += transform.forward * Time.deltaTime * runningSpeed;
+                }
+                else
+                {
+                    rb.position += transform.forward * Time.deltaTime * movementSpeed;
+                    rb.transform.Rotate(0f, -(rotationDegrees) * Time.deltaTime * rotationSpeed, 0f);
+                }
+            }
+
+            //moving forward and rotating to the right simultaneously
+            else if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A))
+            {
+                if (Input.GetKey(KeyCode.Space))
+                {
+                    rb.position += transform.forward * Time.deltaTime * runningSpeed;
+                }
+                else
+                {
+                    rb.position += transform.forward * Time.deltaTime * movementSpeed;
+                    rb.transform.Rotate(0f, rotationDegrees * Time.deltaTime * rotationSpeed, 0f);
+                }
+            }
+            //moving forward
+            else if (Input.GetKey(KeyCode.Space) && !Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+            {
+                if (Input.GetKeyDown(KeyCode.E) && isGrounded)
+                {
+                    rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+                    isGrounded = false;
+                }
+                else
+                {
+                    rb.position += transform.forward * Time.deltaTime * runningSpeed;
+                }
+            }
+            else
+            {
+                rb.position += transform.forward * Time.deltaTime * movementSpeed;
+            }
+        }
+
+        if (Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.Space))
+        {
+            //moving backwards and rotating to the left simultaneously
+            if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+            {
+                rb.position -= transform.forward * Time.deltaTime * backwardsSpeed;
+                rb.transform.Rotate(0f, -(rotationDegrees) * Time.deltaTime * rotationSpeed, 0f);
+            }
+
+            //moving backwards and rotating to the right simultaneously
+            else if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A))
+            {
+                rb.position -= transform.forward * Time.deltaTime * backwardsSpeed;
+                rb.transform.Rotate(0f, rotationDegrees * Time.deltaTime * rotationSpeed, 0f);
+            }
+
+            //moving backwards
+            else
+            {
+                rb.position -= transform.forward * Time.deltaTime * backwardsSpeed;
+            }
+        }
+
+        //turning around
+        else
+        {
+            if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.S))
+            {
+                rb.transform.Rotate(0f, -(rotationDegrees) * Time.deltaTime * rotationSpeed, 0f);
+            }
+
+            if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.S))
+            {
+                rb.transform.Rotate(0f, rotationDegrees * Time.deltaTime * rotationSpeed, 0f);
+            }
+        }
+    }
+}
