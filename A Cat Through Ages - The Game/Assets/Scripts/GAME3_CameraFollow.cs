4254b756650db4f6f215081c58c1e24ea6bb5a8d
Ludwik1526 <mmichal1526@gmail.com>
2019-05-05 12:16:17 +0200

    The 3rd game - movement

---
+++ A Cat Through Ages - The Game/Assets/Scripts/GAME3_CameraFollow.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GAME3_CameraFollow : MonoBehaviour {
+
+    public GameObject player;
+    
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + new Vector3(-25, 285, 125);
+    }
+}
---
+++ A Cat Through Ages - The Game/Assets/Scripts/GAME3_PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GAME3_PlayerMovement : MonoBehaviour {
+
+public float speed;
+private Vector2 velocity;
+private Rigidbody rb;
+
+
+void Start()
+{
+    rb = GetComponent<Rigidbody>();
+}
+
+void Update()
+{
+    Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+
+    velocity = moveInput.normalized * speed * 50;
+
+    Vector3 movement = velocity * Time.fixedDeltaTime;
+
+    rb.MovePosition(rb.position + movement);
+}
+}
+
